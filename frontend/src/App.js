0b41e295dbc6bc34a1a6c2a8c9982fffe4c62a14
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-10 19:15:35 -0800

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import ProductPage from './pages/ProductPage';
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home />
+          <Routes>
+            <Route path='/' element={<Home />} exact></Route>
+            <Route path='/product/:id' element={<ProductPage />}></Route>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
