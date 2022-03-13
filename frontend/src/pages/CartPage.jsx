2477b1141dbc2e446b03c969b4d8ac28d85ccea2
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-12 20:42:07 -0800

    cartpage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,4 +1,37 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Button,
+  ListGroup,
+  Card,
+  Image,
+  Form,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
+
 const CartPage = () => {
+  const dispatch = useDispatch();
+  const params = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
+  const productId = params.id;
+  // pulling qty from url
+  const qty = new URLSearchParams(location.search).get('qty');
+  console.log(qty);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
   return (
     <div>
       <h1>cart</h1>
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -16,7 +16,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const ProductPage = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const productDetails = useSelector((state) => state.productDetails);
