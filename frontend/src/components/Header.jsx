af2d634220ba7bf878f6633ebc91e0ff33aa9f72
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-14 22:06:20 -0700

    fixed navbar username and logout fun

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
   const logoutHandler = () => {
     dispatch(logout());
   };
