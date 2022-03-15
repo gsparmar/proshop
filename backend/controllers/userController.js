83d90ebd90eab85c0a5d1198356a5a15bc6485d7
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-14 22:58:53 -0700

    update profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,11 @@
-const AsyncHandler = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 
 // @ desc Auth user & get token
 // @ route POST /api/users/login
 // @ access Public
-const authUser = AsyncHandler(async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
@@ -27,7 +27,7 @@ const authUser = AsyncHandler(async (req, res) => {
 // @ desc Register a new uesr
 // @ route POST /api/users
 // @ access Public
-const registerUser = AsyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { email, password, name } = req.body;
 
   const userExists = await User.findOne({ email });
@@ -57,10 +57,10 @@ const registerUser = AsyncHandler(async (req, res) => {
   }
 });
 
-// @ desc user profile
+// @ desc get user profile
 // @ route GET /api/users/profile
 // @ access PRIVATE
-const getUserProfile = AsyncHandler(async (req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -76,4 +76,31 @@ const getUserProfile = AsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authUser, getUserProfile, registerUser };
+// @ desc update user profile
+// @ route PUT /api/users/profile
+// @ access PRIVATE
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+module.exports = { authUser, getUserProfile, registerUser, updateUserProfile };
