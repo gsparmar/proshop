fad73a0753ef9206cf9bbedf9e05d299defdebb9
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-16 19:38:37 -0700

    payment page and payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import ShippingPage from './pages/ShippingPage';
+import PaymentPage from './pages/PaymentPage';
 const App = () => {
   return (
     <Router>
@@ -17,6 +18,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
+            <Route path='/payment' element={<PaymentPage />} />
             <Route path='/register' element={<RegisterPage />} />
             <Route path='/shipping' element={<ShippingPage />} />
             <Route path='/profile' element={<ProfilePage />} />
