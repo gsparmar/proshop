dda9276188e6921c6ee72f09f7f5c6e0cb0b57a7
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-19 00:03:38 -0700

    user edit screen & get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentPage from './pages/PaymentPage';
 import PlaceOrderPage from './pages/PlaceOrderPage';
 import OrderPage from './pages/OrderPage';
 import UserListPage from './pages/UserListPage';
+import UserEditPage from './pages/UserEditPage';
 
 const App = () => {
   return (
@@ -33,6 +34,7 @@ const App = () => {
             <Route path='/cart/:id' element={<CartPage />} />
             <Route path='/cart' element={<CartPage />} />
             <Route path='/admin/userlist' element={<UserListPage />} />
+            <Route path='/admin/user/:id/edit' element={<UserEditPage />} />
             <Route path='/' element={<Home />} exact />
           </Routes>
         </Container>
---
+++ frontend/src/pages/UserEditPage.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { getUserDetails } from '../actions/userActions';
+import FormContainer from '../components/FormContainer';
+
+const UserEditPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const params = useParams();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const userId = params.id;
+
+  const userDetails = useSelector((state) => state.userDetails);
+
+  const { loading, error, user } = userDetails;
+
+  useEffect(() => {
+    if (!user.name || user._id !== userId) {
+      dispatch(getUserDetails(userId));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [dispatch, user, userId]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <Link to='/admin/userlist' className='btn btn-light my-3'>
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant='danager'>{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId='name'>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type='name'
+                placeholder='Enter Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='email'>
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='isAdmin'>
+              <Form.Check
+                type='checkbox'
+                label='Is Admin'
+                checked={isAdmin}
+                onChange={(e) => setIsAdmin(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+
+            <Button type='submit' variant='primary' className='my-3'>
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </div>
+  );
+};
+
+export default UserEditPage;
