c3d2a039f7b64201691f393221d4f2004f21b054
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-18 22:26:40 -0700

    admin delete user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
   registerUser,
   updateUserProfile,
   getUsers,
+  deleteUser,
 } = require('../controllers/userController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
@@ -17,5 +18,7 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router.route('/:id').delete(protect, admin, deleteUser);
 
 module.exports = router;
