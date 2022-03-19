e01f300f2678c1a7d8262914dfc21ec20125a601
Gurveer Parmar <gurveerparmar92@gmail.com>
2022-03-18 20:37:03 -0700

    user details and orders reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,10 +10,12 @@ import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
+  USER_DETAILS_RESET,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
 } from '../constants/userConstants';
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -52,10 +54,12 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
-
+// add a reset to clear user details and orders history for profile page
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
